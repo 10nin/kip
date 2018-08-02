2412bbae2e294f2b2bbc77ea0d63edb8a67a877a
Akira Ota 10nin <akira19870221@gmail.com>
2018-08-02 20:25:45 +0900

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to kip
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/kip/core_test.clj
@@ -0,0 +1,7 @@
+(ns kip.core-test
+  (:require [clojure.test :refer :all]
+            [kip.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
