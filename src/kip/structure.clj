51110bf1d90560e41012d80be05b5f1f586b85ba
Akira Ota 10nin <akira19870221@gmail.com>
2018-08-06 00:04:21 +0900

    Add minimal manipulation functions.

--- src/kip/structure.clj
+++ src/kip/structure.clj
@@ -1,4 +1,23 @@
 (ns kip.structure)
 
+; master-ticket-list
+(defonce tickets (atom nil))
 (defrecord Account [id, account-name, e-mail])
-(defrecord Ticket [no, status, person, subject, details])
+(defrecord Ticket [no, status, person, subject, detail])
+
+(defn get-latest-ticket-no []
+  0)
+
+(defn get-ticket-by-no
+  ([ticket-no ticket-list]
+   (when (first @ticket-list)
+     (if (= (:no (first ticket-list)) ticket-no) (first ticket-list)
+         (get-ticket-by-no [ticket-no (rest ticket-list)]))))
+  ([ticket-no]
+   (get-ticket-by-no ticket-no @tickets)))
+
+(defn find-tickets-by-person [p]
+  nil)
+
+(defn make-ticket [person subject detail]
+  (->Ticket (get-latest-ticket-no) 0 person subject detail))
