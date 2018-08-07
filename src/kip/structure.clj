1e2d4144f30b5b4810faff8495e46bc2e46eb05b
Akira Ota 10nin <akira19870221@gmail.com>
2018-08-07 20:59:08 +0900

    Fixed equality check.

--- src/kip/structure.clj
+++ src/kip/structure.clj
@@ -5,14 +5,6 @@
 (defrecord Ticket [no, status, person, subject, detail])
 (defonce tickets (atom nil :validator (fn [a] (or (instance? Ticket a) (nil? a)))))
 
-(defn account-equals [a b]
-  (if (and
-         (= (:id a) (:id b))
-         (= (:account-name a) (:account-name b))
-         (= (:e-mail a) (:e-mail b)))
-    true
-    false))
-
 (defn get-latest-ticket-no []
   0)
 
@@ -27,7 +19,7 @@
 (defn find-tickets-by-account
   ([a ticket-list]
    (when (instance? Account a)
-     (if (account-equals a (:person (first ticket-list)))
+     (if (= a (:person (first ticket-list)))
        (first ticket-list)
        (find-tickets-by-account a (rest ticket-list)))))
    ([a]
