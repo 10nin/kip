2841c7ea24afb4953bbdeef6999ea72db1aa22e0
Akira Ota 10nin <akira19870221@gmail.com>
2018-08-07 17:07:41 +0900

    Implements find-ticket-by-account and add type equality check function.

--- src/kip/structure.clj
+++ src/kip/structure.clj
@@ -5,6 +5,14 @@
 (defrecord Ticket [no, status, person, subject, detail])
 (defonce tickets (atom nil :validator (fn [a] (or (instance? Ticket a) (nil? a)))))
 
+(defn account-equals [a b]
+  (if (and
+         (= (:id a) (:id b))
+         (= (:account-name a) (:account-name b))
+         (= (:e-mail a) (:e-mail b)))
+    true
+    false))
+
 (defn get-latest-ticket-no []
   0)
 
@@ -16,8 +24,14 @@
   ([ticket-no]
    (get-ticket-by-no ticket-no @tickets)))
 
-(defn find-tickets-by-person [p]
-  nil)
+(defn find-tickets-by-account
+  ([a ticket-list]
+   (when (instance? Account a)
+     (if (account-equals a (:person (first ticket-list)))
+       (first ticket-list)
+       (find-tickets-by-account a (rest ticket-list)))))
+   ([a]
+    (find-tickets-by-account a @tickets)))
 
 (defn make-ticket [person subject detail]
   (->Ticket (get-latest-ticket-no) 0 person subject detail))
