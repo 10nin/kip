2fcceffb6ca9b3e0bc927e6b8ed300fe04670388
Akira Ota 10nin <akira19870221@gmail.com>
2018-08-08 20:37:51 +0900

    Add cleanup function and fixed get-last index logic.

--- src/kip/structure.clj
+++ src/kip/structure.clj
@@ -4,15 +4,17 @@
 (defrecord Ticket [no, status, person, subject, detail])
 ; master-ticket-list
 (defonce tickets (atom nil))
+(defonce last-ticket-index (atom 0))
+
+(defn clear-ticket-list []
+  (reset! tickets nil)
+  (reset! last-ticket-index 0))
 
 (defn ticket? [t]
   "Ticket is must need assinged account and subject"
   (and (instance? Ticket t)
        (instance? Account (:person t))
        (< 0 (count (:subject t)))))
-
-(defn get-latest-ticket-no []
-  0)
 
 (defn get-ticket-by-no
   ([ticket-no ticket-list]
@@ -32,7 +34,9 @@
     (find-tickets-by-account a @tickets)))
 
 (defn make-ticket [person subject detail]
-  (->Ticket (get-latest-ticket-no) 0 person subject detail))
+  (->Ticket @last-ticket-index 0 person subject detail))
 
 (defn add-ticket [t]
-  (if (ticket? t) (swap! tickets conj t)))
+  (when (ticket? t)
+    (swap! tickets conj t)
+    (reset! last-ticket-index (+ @last-ticket-index 1))))
