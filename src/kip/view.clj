d045172b6db98885aad04ece4e14a8c348002a77
Akira Ota 10nin <akira19870221@gmail.com>
2018-08-14 15:00:29 +0900

    Add make html table function.

--- src/kip/view.clj
+++ src/kip/view.clj
@@ -7,5 +7,7 @@
   "get hiccup style table from 'ticket-summary'.
 ticket-summary contains {ticket-no, ticket-status, assined-account-name, ticket-subject}, it is not Ticket structure."
   (ht/to-table1d ticket-summary
-                [:no "#", :status "Status", :assign "Assin-to", :subject "Subject"]))
+                 [:no "#", :status "Status", :assign "Assin-to", :subject "Subject"]))
 
+(defn make-summary-html-table [ticket-summary]
+  (hc/html (gen-summary-table ticket-summary)))
